9baeb752e27aafa14682aa3d4815ce66ff0c1673
Hernán Moreno <hernan.alexis.moreno@gmail.com>
2020-03-29 00:01:33 -0300

    listo para producción

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Producto } from "../interfaces/producto.interface";
-import { resolve } from "../../../node_modules/@types/q";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/productos.interface';
+import { resolve } from '../../../node_modules/@types/q';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class ProductosService {
   cargando = true;
@@ -18,7 +18,7 @@ export class ProductosService {
   private cargarProductos() {
     return new Promise((resolve, reject) => {
       this.http
-        .get("https://angular-html-25cf9.firebaseio.com/productos_idx.json")
+        .get('https://angular-html-25cf9.firebaseio.com/productos_idx.json')
         .subscribe((resp: Producto[]) => {
           this.productos = resp;
           this.cargando = false;
